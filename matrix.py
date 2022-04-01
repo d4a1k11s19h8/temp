8409f96ba91c7dbd12aa5969736200ab32eb02a8
d4a1k11s19h8 <102427065+d4a1k11s19h8@users.noreply.github.com>
2022-04-01 13:05:37 +0530

    Add files via upload

--- matrix.py
+++ matrix.py
@@ -3,6 +3,8 @@ x=numpy.array([[2,1,1],
                [3,2,1],
                [2,1,2]])
 print(dir(numpy))
+
+'''to copy a matrix'''
 y=x.copy()
 
 '''for taking the traspose'''
@@ -11,9 +13,9 @@ y=x.transpose()
 '''for multiplying corresponding elements of an array'''
 print(numpy.multiply(x,y))
 
-'''for matrix multiplication'''
-print(numpy.matmul(x,y))
-y.argsort()
+'''for matrix multiplication can use any of 2'''
+print(numpy.matmul(x,y),x.dot(y))
+
 #x.reshape(9,1)
 y=numpy.linalg.inv(x)
 print(numpy.matmul(x,y),numpy.__version__)
@@ -74,35 +76,49 @@ newarr = arr.astype('i')
 to change the data type of an existing array,
 is to make a copy of the array with the astype() method.'''
 
+'''to delete an element from array when index is given'''
+print(numpy.delete(x,5 #<index>
+                   ),x)
+
+# dtype is not required argument
+print(numpy.zeros((3,3))) # this gives 0. as all the elements
+print(numpy.zeros((3,3),dtype='i'))# this gives 0 as all the elements
+
+'''to create a void matrix it takes binary object
+encode() func makes an object in binary lang'''
+x1=numpy.void((str(3).encode(),str(3).encode()))
+
+'''to print average of all the numbers'''
+print(numpy.average(x))
+
+'''to know the size of an array'''
+print(numpy.size(x))
+
+'''to split the array in no of divisions'''
+print(numpy.split(x,3))
+
+'''to add two arrays can use any of 2'''
+print(numpy.add(x,y),x+y)
+
+'''to add two arrays and change the datatype''' 
+print(numpy.array(numpy.add(x,y),dtype='i'))
+print(numpy.array(numpy.add(x,y),dtype=numpy.int32))
 
 
+print(numpy.append(x,[1,2,3]))
+
+'''to change the size of an array'''
+x.resize(9,1)
+print(x)
 
 
+'''using numpy.arange(a,b,c) for creating an array
+where a,b,c has usual meaning as in range() func
 
+reshape changes the size of an array
+'''
+print(numpy.arange(1,25,2))
+print(numpy.arange(12).reshape(3,4))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(numpy.conjugate(y),y)
 
